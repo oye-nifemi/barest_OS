379d4a4e4e9f6a09a43a366b8ab9dab90c401415
oye-nifemi <nifoyewole@gmail.com>
2023-05-11 15:46:46 +0100

    following lecturer

--- the_bare_os/src/main.rs
+++ the_bare_os/src/main.rs
@@ -6,7 +6,29 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World! This is just a quick illustration";
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum Colour {
+ Black = 0,
+ Blue = 1,
+ Green = 2,
+ Cyan = 3,
+ Red = 4,
+ Magenta = 5,
+ Brown = 6,
+ LightGray = 7,
+ DarkGray = 8,
+ LightBlue = 9,
+ LightGreen = 10,
+ LightCyan = 11,
+ LightRed = 12,
+ Pink = 13,
+ Yellow = 14,
+ White = 15,
+}
+
+static HELLO: &[u8] = b"Hello World! This is just a quick illustration Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -15,9 +37,10 @@ pub extern "C" fn _start() -> ! {
     framebuffer.offset(1).write_volatile(0x30);
     }*/
     for (i, &byte) in HELLO.iter().enumerate() {
+        let i = 40 + i;
         unsafe {
             *framebuffer.offset(i as isize * 2) = byte;
-            *framebuffer.offset(i as isize * 2 + 1) = 0xb;
+            *framebuffer.offset(i as isize * 2 + 1) = Colour::LightGreen as u8;
         }
     }
     loop {}
